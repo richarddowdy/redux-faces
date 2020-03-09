ac8fa59ac3916ba2515d2c6e4b164b8279ed0b5d
Richard Dowdy <richard.dowdy0@gmail.com>
2020-03-09 11:09:26 -0700

    added background color change with face change on button clicks

--- rootReducer.js
+++ rootReducer.js
@@ -1,23 +1,26 @@
-const INITIAL_STATE = { face: "⊂((・⊥・))⊃" };
+const INITIAL_STATE = { face: "⊂((・⊥・))⊃", background: "grey"};
 
 function rootReducer (state=INITIAL_STATE, action){
+  console.log(action.type);
   switch(action.type){
     case "happy":
-      return { ...state, face: "(＾ω＾)"};
+      return { ...state, face: "(＾ω＾)", background: "yellow"};
 
     case "sad":
-      return { ...state, face: "ಠ⌣ಠ"};
+      return { ...state, face: "ಠ⌣ಠ", background: "blue"};
 
     case "angry":
-      return { ...state, face: "⋋_⋌"};
+      return { ...state, face: "⋋_⋌", background: "red"};
 
     case "confused":
-      return { ...state, face: "(⊙_☉)"};
+      return { ...state, face: "(⊙_☉)", background: "green"};
 
     default :
       return state;
   }
 };
+
+
 
 const store = Redux.createStore(rootReducer);
 
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // import store from "./rootReducer";
 
 window.onload = function () {
+  // const body = document.getElementsByTagName("body");
   const faceElement = document.getElementById("face");
   const happyButton = document.getElementById("happy");
   const sadButton = document.getElementById("sad");
@@ -8,26 +9,39 @@ window.onload = function () {
   const confusedButton = document.getElementById("confused");
 
   const initialFace = store.getState().face;
+  const initialColor = store.getState().background;
   faceElement.innerHTML = initialFace;
+  document.body.style.backgroundColor = initialColor
 
+  function handleChange(faceElement){
+    const currentFace = store.getState().face;
+    const color = store.getState().background;
+    faceElement.innerHTML = currentFace;
+    document.body.style.backgroundColor = color;
+  }
+  store.subscribe( () => handleChange(faceElement) );
+  
   happyButton.addEventListener("click", function(){
     store.dispatch({ type: "happy" });
-    const currentFace = store.getState().face;
-    faceElement.innerHTML = currentFace;
+    // const currentFace = store.getState().face;
+    // faceElement.innerHTML = currentFace;
   });
+
   sadButton.addEventListener("click", function(){
     store.dispatch({ type: "sad" });
-    const currentFace = store.getState().face;
-    faceElement.innerText = currentFace;
+    // const currentFace = store.getState().face;
+    // faceElement.innerText = currentFace;
   });
+
   angryButton.addEventListener("click", function(){
     store.dispatch({ type: "angry" });
-    const currentFace = store.getState().face;
-    faceElement.innerText = currentFace;
+    // const currentFace = store.getState().face;
+    // faceElement.innerText = currentFace;
   });
+
   confusedButton.addEventListener("click", function(){
     store.dispatch({ type: "confused" });
-    const currentFace = store.getState().face;
-    faceElement.innerText = currentFace;
+    // const currentFace = store.getState().face;
+    // faceElement.innerText = currentFace;
   });
 };
